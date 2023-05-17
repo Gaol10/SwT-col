984df7bfafaecd39c0f89d825cb2b3aa9775dbc3
Gaol10 <128594152+Gaol10@users.noreply.github.com>
2023-05-17 10:40:01 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,216 @@
+import os
+import argparse
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.cuda.amp import autocast, GradScaler
+from torch.utils.data import DataLoader
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+import numpy as np
+import cv2
+
+from utils import AverageMeter, hwc_to_chw, to_lab
+from datasets.loader import PairLoader
+from models import *
+
+# ablation L1Loss->MSELoss()
+# PSNR: 29.82	SSIM: 0.918
+#     		->CrossEntropyLoss
+# NaN
+
+# norm
+# a) remove the norm before MHSA
+# b) add normbefore MHSA
+
+# parallel convolution
+# a) remove the parallel conv ->PSNR:30.34 SSIM:0.922
+# b) replace parallel V with X ->PSNR:30.37 SSIM->0.921
+# c) Parallel DWConv on X ->PSNR:30.19 SSIM:0.921
+# d) OWConv before MLP->PSNR:30.51 SSIM:0.922
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', default='dehazeformer-l', type=str, help='model name')
+parser.add_argument('--num_workers', default=4, type=int, help='number of workers')
+parser.add_argument('--no_autocast', action='store_false', default=True, help='disable autocast')
+parser.add_argument('--save_dir', default='./saved_models/', type=str, help='path to models saving')
+parser.add_argument('--data_dir', default='./data/', type=str, help='path to dataset')
+parser.add_argument('--log_dir', default='./logs/', type=str, help='path to logs')
+parser.add_argument('--dataset', default='DATASET', type=str, help='dataset name')
+parser.add_argument('--gpu', default='0,1', type=str, help='GPUs used for training')
+args = parser.parse_args()
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+device = torch.device("cuda")
+
+
+def train(train_loader, network, criterion, optimizer, scaler):
+    train_count = 0
+
+    losses = AverageMeter()
+
+    torch.cuda.empty_cache()
+
+    network.train()
+
+    for batch in train_loader:  # 此处才读GT
+
+        train_count += 1
+
+        target_img = batch['target'].cuda()
+        source_img = batch['source'].cuda()
+        # print(batch['filename'])
+
+        # target_lab = hwc_to_chw(target_lab)
+        # source_lab = hwc_to_chw(source_lab)# copy() already....
+        # source_lab=torch.tensor(source_lab).unsqueeze(0)
+        # target_lab = torch.tensor(target_lab).unsqueeze(0).cuda()
+
+        with autocast(args.no_autocast):
+            output = network(source_img).to(device)
+            loss = criterion(output, target_img).to(device)
+
+        if train_count % 50 == 0:
+            print("第{}批训练结束，当前loss={}".format(train_count, loss))
+
+        losses.update(loss.item())
+
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+    return losses.avg
+
+
+def valid(val_loader, network):
+    val_count = 0
+
+    PSNR = AverageMeter()
+
+    torch.cuda.empty_cache()
+
+    network.eval()
+
+    for batch in val_loader:
+        val_count += 1
+
+        target_img = batch['target'].cuda()
+        source_img = batch['source'].cuda()
+
+        # target_lab, source_lab = to_lab(target_img)
+        # target_lab = hwc_to_chw(target_lab)
+        # source_lab = hwc_to_chw(source_lab)  # copy() already....
+        # source_lab = torch.tensor(source_lab).unsqueeze(0)
+        # target_lab = torch.tensor(target_lab).unsqueeze(0).cuda()
+
+        with torch.no_grad():  # torch.no_grad() may cause warning
+            output = network(source_img).clamp_(-1, 1)
+
+        mse_loss = F.mse_loss(output * 0.5 + 0.5, target_img * 0.5 + 0.5, reduction='none').mean((1, 2, 3))
+        psnr = 10 * torch.log10(1 / mse_loss).mean()
+        if val_count % 50 == 0:
+            print("第{}批验证结束，当前psnr={}".format(val_count, psnr))
+        PSNR.update(psnr.item(), source_img.size(0))
+
+    return PSNR.avg
+
+
+if __name__ == '__main__':
+    setting_filename = os.path.join('configs', args.model + '.json')
+    if not os.path.exists(setting_filename):
+        setting_filename = os.path.join('configs', 'default.json')
+    with open(setting_filename, 'r') as f:
+        setting = json.load(f)
+
+    network = eval(args.model.replace('-', '_'))()
+    network = nn.DataParallel(network).cuda()
+
+    criterion = nn.L1Loss().cuda()
+
+    if setting['optimizer'] == 'adam':
+        optimizer = torch.optim.Adam(network.parameters(), lr=setting['lr'])
+    elif setting['optimizer'] == 'adamw':
+        optimizer = torch.optim.AdamW(network.parameters(), lr=setting['lr'])
+    else:
+        raise Exception("ERROR: unsupported optimizer")
+
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=setting['epochs'],
+                                                           eta_min=setting['lr'] * 1e-2)
+    scaler = GradScaler()
+
+    dataset_dir = os.path.join(args.data_dir, args.dataset)
+    train_dataset = PairLoader(dataset_dir, 'train', 'train',
+                               setting['patch_size'], setting['edge_decay'], setting['only_h_flip'])
+    train_loader = DataLoader(train_dataset,
+                              batch_size=setting['batch_size'],
+                              shuffle=True,
+                              num_workers=args.num_workers,
+                              pin_memory=True,
+                              drop_last=True)
+    val_dataset = PairLoader(dataset_dir, 'test', setting['valid_mode'],
+                             setting['patch_size'])
+    val_loader = DataLoader(val_dataset,
+                            batch_size=setting['batch_size'],
+                            num_workers=args.num_workers,
+                            pin_memory=True)
+
+    save_dir = os.path.join(args.save_dir, args.exp)
+    os.makedirs(save_dir, exist_ok=True)
+
+    # checkpoints
+    checkpoint_path = os.path.join(args.log_dir, args.model)
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+
+    # load the checkpoints
+    # latest_checkpoints = torch.load('./logs/indoor/dehazeformer-l_epoch_340.pt')
+    # network.load_state_dict(latest_checkpoints['model_state_dict'])
+    # optimizer.load_state_dict(latest_checkpoints['optimizer_state_dict'])
+    # epo = latest_checkpoints['epoch']+1
+    # if we want to restore the training we delete 'epo = 0' which is for a new training
+    epo = 0
+
+    if not os.path.exists(os.path.join(save_dir, args.model + '.pth')):
+        print('==> Start training, current model name: ' + args.model)
+        # print(network)
+        # writer = SummaryWriter(log_dir=os.path.join(args.log_dir, args.exp, args.model))
+        writer = SummaryWriter(logdir=checkpoint_path)
+
+        best_psnr = 0
+        for epoch in tqdm(range(epo, setting['epochs'])):
+            print("---------the beginning of train epoch:{}---------".format(epoch))
+
+            loss = train(train_loader, network, criterion, optimizer, scaler)
+
+            writer.add_scalar('train_loss', loss, epoch)
+
+            if epoch % 10 == 0:
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': network.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss
+                }, checkpoint_path + '_epoch_' + str(epoch) + '.pt')
+
+            scheduler.step()
+
+            if epoch % setting['eval_freq'] == 0:
+                avg_psnr = valid(val_loader, network)
+
+                writer.add_scalar('valid_psnr', avg_psnr, epoch)
+
+                if avg_psnr > best_psnr:
+                    best_psnr = avg_psnr
+                    torch.save({'state_dict': network.state_dict()},
+                               os.path.join(save_dir, args.model + '.pth'))
+
+                writer.add_scalar('best_psnr', best_psnr, epoch)
+            print("---------the end of training epoch:{}---------".format(epoch))
+            print("---loss:{} psnr:{}------- ".format(loss, avg_psnr))
+
+    else:
+        print('==> Existing trained model')
+        exit(1)
