3d04803d9c2b66ae8b1371fa6d1949b963f7750a
Gaol10 <128594152+Gaol10@users.noreply.github.com>
2023-05-24 16:55:10 +0800

    loader.py

--- datasets/loader.py
+++ datasets/loader.py
@@ -4,39 +4,41 @@ import numpy as np
 import cv2
 
 from torch.utils.data import Dataset
-from utils import hwc_to_chw, read_img
+from utils import hwc_to_chw, read_img,read_img_grey
+
 
 
 def augment(imgs=[], size=256, edge_decay=0., only_h_flip=False):
-	H, W, _ = imgs[0].shape
-	Hc, Wc = [size, size]
+    H, W, _ = imgs[0].shape
+    Hc, Wc = [size, size]
 
-	# simple re-weight for the edge
-	if random.random() < Hc / H * edge_decay:
-		Hs = 0 if random.randint(0, 1) == 0 else H - Hc
-	else:
-		Hs = random.randint(0, H-Hc)
+    # simple re-weight for the edge
+    if random.random() < Hc / H * edge_decay:
+        Hs = 0 if random.randint(0, 1) == 0 else H - Hc
+    else:
+        Hs = random.randint(0, H - Hc)
 
-	if random.random() < Wc / W * edge_decay:
-		Ws = 0 if random.randint(0, 1) == 0 else W - Wc
-	else:
-		Ws = random.randint(0, W-Wc)
+    if random.random() < Wc / W * edge_decay:
+        Ws = 0 if random.randint(0, 1) == 0 else W - Wc
+    else:
+        Ws = random.randint(0, W - Wc)
 
-	for i in range(len(imgs)):
-		imgs[i] = imgs[i][Hs:(Hs+Hc), Ws:(Ws+Wc), :]
+    for i in range(len(imgs)):
+        imgs[i] = imgs[i][Hs:(Hs + Hc), Ws:(Ws + Wc), :]
 
-	# horizontal flip
-	if random.randint(0, 1) == 1:
-		for i in range(len(imgs)):
-			imgs[i] = np.flip(imgs[i], axis=1)
+    # horizontal flip
+    if random.randint(0, 1) == 1:
+        for i in range(len(imgs)):
+            imgs[i] = np.flip(imgs[i], axis=1)
 
-	if not only_h_flip:
-		# bad data augmentations for outdoor
-		rot_deg = random.randint(0, 3)
-		for i in range(len(imgs)):
-			imgs[i] = np.rot90(imgs[i], rot_deg, (0, 1))
-			
-	return imgs
+    if not only_h_flip:
+        # bad data augmentations for outdoor
+        rot_deg = random.randint(0, 3)
+        for i in range(len(imgs)):
+            imgs[i] = np.rot90(imgs[i], rot_deg, (0, 1))
+
+    return imgs
+
 
 def align(imgs=[], size=256):
 	H, W, _ = imgs[0].shape
@@ -53,11 +55,11 @@ def align(imgs=[], size=256):
 class PairLoader(Dataset):
 	def __init__(self, data_dir, sub_dir, mode, size=256, edge_decay=0, only_h_flip=False):
 		assert mode in ['train', 'valid', 'test']
-
 		self.mode = mode
 		self.size = size
 		self.edge_decay = edge_decay
 		self.only_h_flip = only_h_flip
+
 
 		self.root_dir = os.path.join(data_dir, sub_dir)
 		self.img_names = sorted(os.listdir(os.path.join(self.root_dir, 'GT')))
@@ -72,15 +74,15 @@ class PairLoader(Dataset):
 
 		# read image, and scale [0, 1] to [-1, 1]
 		img_name = self.img_names[idx]
-		source_img = read_img_gray(os.path.join(self.root_dir, 'gray', img_name)) * 2 - 1
+		source_img = read_img_grey(os.path.join(self.root_dir, 'gray', img_name)) * 2 - 1
 		target_img = read_img(os.path.join(self.root_dir, 'GT', img_name)) * 2 - 1
-		
+
 		if self.mode == 'train':
-			[target_img, source_img] = augment([target_img, source_img], self.size, self.edge_decay, self.only_h_flip)
+			[target_img,source_img] = augment([target_img,source_img], self.size, self.edge_decay, self.only_h_flip)
 
 		if self.mode == 'valid':
-			[target_img, source_img] = align([target_img, source_img], self.size)
+			[target_img,source_img] = align([target_img,source_img], self.size)
 
-		return {'target': target_img,'source':source_img, 'filename': img_name}
-		# C 通道数 w 宽 h 高
+
+		return {'target': hwc_to_chw(target_img),'source':hwc_to_chw(source_img), 'filename': img_name}
 
