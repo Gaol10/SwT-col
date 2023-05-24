ecba976669023b8a11d53c6c2dc11db5ea000da7
Gaol10 <128594152+Gaol10@users.noreply.github.com>
2023-05-24 12:12:06 +0800

    __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from .common import AverageMeter, ListAverageMeter, read_img, write_img, hwc_to_chw, chw_to_hwc,read_img_grey
+from .common import AverageMeter, ListAverageMeter, read_img, write_img, hwc_to_chw, chw_to_hwc,read_img_gray
 from .data_parallel import BalancedDataParallel
